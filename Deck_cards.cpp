27d349c69046da9f4b1bc20230733a249778c847
Gerorge Liao <george.liao@canonical.com>
2023-05-23 00:02:57 +0200

    clang-format Deck_cards.cpp

--- Deck_cards.cpp
+++ Deck_cards.cpp
@@ -5,15 +5,14 @@
 //
 //*****************************************************************
 
-
+#include "Card.cpp"
 #include <iostream>
 #include <string>
-#include "Card.cpp"
 
 class Deck_cards
 {
     Card *deck;
-    int number_ofcards;//track how many cards in the deck
+    int number_ofcards; //track how many cards in the deck
 
     static const int SUITS_PER_DECK = 4;
     static const int CARDS_PER_SUIT = 13;
@@ -24,10 +23,10 @@ public:
     void shuffle();
 
     //other operations besides shuffling
-    void add_card(Card newcard);//add one card to the deck if it is not full
-    Card get_top_card();//deal one card
-    Card peek_top_card() const;//just peek the top card and put it back
-    void cut_deck(){}//cut the deck into two, not implemented yet
+    void add_card(Card newcard); //add one card to the deck if it is not full
+    Card get_top_card();         //deal one card
+    Card peek_top_card() const;  //just peek the top card and put it back
+    void cut_deck() {}           //cut the deck into two, not implemented yet
 
     void print_deck_cards() const;
     ~Deck_cards();
@@ -39,7 +38,7 @@ Deck_cards::Deck_cards()
     number_ofcards = CARDS_PER_DECK;
 
     for (int i = 0; i < CARDS_PER_DECK; i++) {
-        deck[i] = Card(Rank(i%13), Suit(i/13));
+        deck[i] = Card(Rank(i % 13), Suit(i / 13));
     }
 }
 
@@ -50,11 +49,10 @@ void Deck_cards::shuffle()
 
     srand(time(NULL));
     for (int i = 0; i < CARDS_PER_DECK; i++) {
-        int j = rand()%CARDS_PER_DECK;
+        int j = rand() % CARDS_PER_DECK;
         swap(deck[i], deck[j]);
     }
 }
-
 
 void Deck_cards::print_deck_cards() const
 {
@@ -66,43 +64,40 @@ void Deck_cards::print_deck_cards() const
 
 Deck_cards::~Deck_cards()
 {
-    delete [] deck;
+    delete[] deck;
 }
 
-void Deck_cards::add_card(Card newcard){
-
-    if(number_ofcards >= CARDS_PER_DECK)
-    {
+void Deck_cards::add_card(Card newcard)
+{
+    if (number_ofcards >= CARDS_PER_DECK) {
         throw "Attempt to add a card to full deck";
-    }
-    else
-    {
+    } else {
         deck[number_ofcards] = newcard;
         number_ofcards++;
     }
 }
 
-Card Deck_cards::get_top_card(){
-//    deal the shuffled cards
+Card Deck_cards::get_top_card()
+{
+    //    deal the shuffled cards
     Card temporary;
-    if(number_ofcards > 0){
-        temporary = deck[number_ofcards-1];
+    if (number_ofcards > 0) {
+        temporary = deck[number_ofcards - 1];
         number_ofcards--;
-    }
-    else {
+    } else {
         throw "try to get a card, but there is no card left";
     }
     return temporary;
 }
 
-Card Deck_cards::peek_top_card() const{
-//    peek the top card
+Card Deck_cards::peek_top_card() const
+{
+    //    peek the top card
 
     Card temporary;
-    if(number_ofcards > 0){
-        temporary = deck[number_ofcards-1];
-    }
-    else {
+    if (number_ofcards > 0) {
+        temporary = deck[number_ofcards - 1];
+    } else {
         throw "try to peek a card, but there is no card left";
     }
     return temporary;
